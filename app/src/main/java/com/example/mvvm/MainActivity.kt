f6004271cc96a830080b7f845f1cab06a62185f9
Teo Grossi <teohsgrossi@hotmail.com>
2023-11-06 23:34:39 +0100

    correcion de vista

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -35,8 +35,8 @@ class MainActivity : ComponentActivity() {
             val viewModel: MainActivityViewModel = viewModel()
             MVVMTheme {
                 Surface {
-                    AppNavigation()git status
-                    
+                    AppNavigation()
+
 
                 }
 
